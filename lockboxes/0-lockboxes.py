19d12754f40592ef62f9bf5b1ea5dfcdc1a76087
gonzalopedernera <gonzalopeder2@gmail.com>
2024-08-28 15:49:43 -0400

    Pleasing PEP 8

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -3,39 +3,38 @@
 Task for interview preparation: lockboxes algorithm
 """
 def canUnlockAll(boxes):
-    #if the first box is empty
+
+    # if the first box is empty
     if len(boxes[0]) < 1:
         return False
     flag = True
-    #dictionary to track open boxes
+    # Dictionary to track open boxes
     toBeOpened = {}
     for x in range(len(boxes)):
         toBeOpened[x] = False
     toBeOpened[0] = True
 
-    #Store keys on an array and a copy
+    # Store keys on an array and a copy
     keys = boxes[0]
 
-    #iterate the keys
+    # Iterate the keys
     for key in keys:
-        #if the key can open a box
+        # If the key can open a box
         if key in toBeOpened:
 
-            #if the box is not already open
-            if toBeOpened[key] == False:
+            # If the box is not already open
+            if not toBeOpened[key]:
                 toBeOpened[key] = True
 
-                #Add the keys from the newly open box to the keys array
+                # Add the keys from the newly open box to the keys array
                 for newKey in boxes[key]:
                     if 0 < newKey <= len(boxes) and newKey not in keys:
                         keys.append(newKey)
-                
-    
-    #Check if all the boxes have been opened
+
+    # Check if all the boxes have been opened
     for key in toBeOpened:
 
-        #In case a box is not open, the return flag will be false
-        if toBeOpened[key] == False:
+        # In case a box is not open, the return flag will be false
+        if not toBeOpened[key]:
             flag = False
     return flag
-
